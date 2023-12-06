8ac6ca21eebe04ab33802f0ea530d632594f9224
simon-mo <simon.mo@hey.com>
2023-12-06 20:53:47 +0000

    working request to buffer copy

--- app.py
+++ app.py
@@ -1,85 +1,106 @@
-import requests
 import time
-import httpx
-import asyncio
-from model_loader import fast_download, cuda_malloc
+import struct
+
+import requests
 import torch
 
+from model_loader import download_to_device
 
-async def download_url(url, num_workers: int):
+def get_safe_tensor_metadata(url):
     # resolve any redirect
     resp = requests.head(url, allow_redirects=True)
-    final_url = resp.url
+
+    resolved_url = resp.url
     content_size = resp.headers.get("Content-Length")
 
-    return fast_download(final_url)
+    # Fetch the first 8 bytes of the file
+    headers = {'Range': 'bytes=0-7'}
+    response = requests.get(resolved_url, headers=headers)
+    # Interpret the bytes as a little-endian unsigned 64-bit integer
+    length_of_header = struct.unpack('<Q', response.content)[0]
+    # Fetch length_of_header bytes starting from the 9th byte
+    headers = {'Range': f'bytes=8-{7 + length_of_header}'}
+    response = requests.get(url, headers=headers)
+    # Interpret the response as a JSON object
+    tensor_header = response.json()
 
-    buffers = []
+    return {
+        "resolved_url": resolved_url,
+        "content_size": content_size,
+        "header_size": length_of_header + 8,
+        "data_size": int(content_size) -length_of_header - 8,
+        "tensor_header": tensor_header,
+    }
 
-    async with httpx.AsyncClient() as client:
-        tasks = []
-        start = 0
-        chunksize = int(content_size) // num_workers
-
-        for i in range(num_workers):
-            if i == num_workers - 1:
-                chunksize = int(content_size) - start
-            tasks.append(
-                asyncio.create_task(
-                    client.get(
-                        final_url,
-                        headers={"Range": f"bytes={start}-{start + chunksize}"},
-                    )
-                )
-            )
-            start += chunksize
-
-        resps = await asyncio.gather(*tasks)
-        for resp in resps:
-            buffers.append(resp.content)
-
-    total_bytes = sum(map(len, buffers))
-    return total_bytes
-
-
-class Loader:
-    def __init__(self):
-        ...
-
-    def mmap(self, url):
-        total_bytes = asyncio.run(download_url(url, 4))
-        print(f"Downloaded {total_bytes} bytes")
-
-
-class LoaderTensor:
-    def __init__(self, ptr):
+class LoaderByteTensor:
+    def __init__(self, ptr, nbytes):
         self.ptr = ptr
+        self.nbytes = nbytes
 
     @property
     def __cuda_array_interface__(self):
         return {
-            "data": (self.ptr, False),
-            "shape": (16,),
+            "data": (self.ptr, False), # second item is read-only flag
+            "shape": (self.nbytes,),
             "typestr": "|u1",
         }
 
+class SafeTensorLoader:
+    def __init__(self, url):
+        self.metadata = get_safe_tensor_metadata(url)
+        print(self.metadata)
+        self.raw_pointer = download_to_device(self.metadata["resolved_url"], self.metadata["header_size"], self.metadata["data_size"])
+        self.data = self._create_torch_tensors(self.metadata, self.raw_pointer)
+
+    def _create_torch_tensors(self, metadata, raw_pointer):
+        data = {}
+        for key, value in metadata["tensor_header"].items():
+            if key == "__metadata__":
+                continue
+
+            dtype = value["dtype"]
+            shape = value["shape"]
+            start_offset, end_offset = value["data_offsets"]
+
+            torch_dtype = {
+                "BF16": torch.bfloat16,
+                "F16": torch.float16,
+                "F32": torch.float32,
+            }[dtype]
+
+            # create torch tensor from pointer
+            ptr = raw_pointer + start_offset
+            our_tensor = LoaderByteTensor(ptr, nbytes=end_offset-start_offset)
+            untyped_storage = torch.as_tensor(our_tensor, device=torch.device("cuda")).untyped_storage()
+            torch_tensor = torch.tensor(untyped_storage, device=torch.device("cuda"), dtype=torch_dtype).view(shape)
+
+            assert torch_tensor.numel() == (end_offset-start_offset) // torch_tensor.element_size()
+            assert torch_tensor.data_ptr() == ptr
+
+            data[key] = torch_tensor
+
+        return data
+
+    def keys(self):
+        return self.data.keys()
+
+    def get_tensor(self, key):
+        return self.data[key]
+
+
 if __name__ == "__main__":
-    ptr = cuda_malloc(16)
-    print(f"Python: got ptr: {ptr}")
+    url = "https://huggingface.co/TinyLlama/TinyLlama-1.1B-Chat-v0.6/resolve/main/model.safetensors"
 
-    # create torch tensor from pointer
-    our_tensor = LoaderTensor(ptr)
-    torch_tensor = torch.as_tensor(our_tensor, device=torch.device("cuda"), dtype=torch.uint8)
-    torch_tensor_ptr = torch_tensor.data_ptr()
-    print(f"Python: got tensor: {torch_tensor}, ptr: {torch_tensor_ptr}")
+    loader = SafeTensorLoader(url)
+    print(loader.keys())
+
+    import safetensors
+    with safetensors.safe_open("model.safetensors", "pt", device="cuda") as f:
+        all_tensor_names = set(loader.keys()) - {"__metadata__"}
+        assert set(f.keys()) == set(loader.keys()), f"Keys don't match: {f.keys()} vs {loader.keys()}, missing {all_tensor_names - set(f.keys())}"
+
+        for key in all_tensor_names:
+            assert torch.allclose(f.get_tensor(key), loader.get_tensor(key))
+            print(f"{key} OK")
 
 
-
-    # url = "https://huggingface.co/TinyLlama/TinyLlama-1.1B-Chat-v0.6/resolve/main/model.safetensors"
-
-    # loader = Loader()
-
-    # start = time.perf_counter_ns()
-    # loader.mmap(url)
-    # duration = time.perf_counter_ns() - start
-    # print(f"Downloaded in {duration / 1e9} seconds")
--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,125 @@
+use std::ffi::c_void;
+
 use cudarc::driver::sys as cu;
+use indicatif::ProgressBar;
 use pyo3::prelude::*;
+use tokio_stream::StreamExt;
 
-#[tokio::main]
-async fn download_url(url: &str) -> Result<Vec<u8>, reqwest::Error> {
-    let resp = reqwest::get(url).await?;
-    let body = resp.bytes().await?;
-    Ok(body.to_vec())
-}
-
-#[pyfunction]
-fn fast_download(url: &str) -> PyResult<String> {
-    Ok(download_url(url).unwrap().len().to_string())
-}
-
-#[pyfunction]
-fn cuda_malloc(size: usize) -> PyResult<usize> {
+fn cuda_init() {
     unsafe {
         let result = cu::cuInit(0);
-        assert!(result == cu::cudaError_enum::CUDA_SUCCESS, "init failed: {:?}", result);
+        assert!(
+            result == cu::cudaError_enum::CUDA_SUCCESS,
+            "init failed: {:?}",
+            result
+        );
 
         let mut device: cu::CUdevice = 0;
         let device_result = cu::cuDeviceGet(&mut device, 0);
-        assert!(device_result == cu::cudaError_enum::CUDA_SUCCESS, "device get failed: {:?}", device_result);
+        assert!(
+            device_result == cu::cudaError_enum::CUDA_SUCCESS,
+            "device get failed: {:?}",
+            device_result
+        );
 
         let mut context: cu::CUcontext = std::ptr::null_mut();
         let context_result = cu::cuCtxCreate_v2(&mut context, 0, device);
-        assert!(context_result == cu::cudaError_enum::CUDA_SUCCESS, "context create failed: {:?}", context_result);
+        assert!(
+            context_result == cu::cudaError_enum::CUDA_SUCCESS,
+            "context create failed: {:?}",
+            context_result
+        );
+    }
+}
 
+fn cuda_malloc_pair(size: usize) -> (*mut c_void, u64) {
+    unsafe {
         let mut ptr = std::ptr::null_mut();
-        let alloc_result = cu::cuMemHostAlloc(&mut ptr, size, cu::CU_MEMHOSTALLOC_PORTABLE | cu::CU_MEMHOSTALLOC_WRITECOMBINED);
-        assert!(alloc_result == cu::cudaError_enum::CUDA_SUCCESS, "alloc failed: {:?}", alloc_result);
-
-        println!("Allocated {} bytes at {:p} in host", size, ptr);
-
-        // write 1s to the ptr
-        let ones = vec![1u8; size];
-        std::ptr::copy(ones.as_ptr(), ptr as *mut u8, size);
+        let alloc_result = cu::cuMemHostAlloc(
+            &mut ptr,
+            size,
+            cu::CU_MEMHOSTALLOC_PORTABLE | cu::CU_MEMHOSTALLOC_WRITECOMBINED,
+        );
+        assert!(
+            alloc_result == cu::cudaError_enum::CUDA_SUCCESS,
+            "host alloc failed: {:?}",
+            alloc_result
+        );
 
         // alloc device memory
         let mut device_ptr = cu::CUdeviceptr::default();
         let device_alloc_result = cu::cuMemAlloc_v2(&mut device_ptr, size);
-        assert!(device_alloc_result == cu::cudaError_enum::CUDA_SUCCESS, "device alloc failed: {:?}", device_alloc_result);
+        assert!(
+            device_alloc_result == cu::cudaError_enum::CUDA_SUCCESS,
+            "device alloc failed: {:?}",
+            device_alloc_result
+        );
 
-        // copy from host to device
-        let copy_result = cu::cuMemcpyHtoD_v2(device_ptr, ptr, size);
-        assert!(copy_result == cu::cudaError_enum::CUDA_SUCCESS, "copy failed: {:?}", copy_result);
-
-        // return the device ptr
-        println!("Copied {} bytes to {:?} in device", size, device_ptr);
-
-        Ok(device_ptr as usize)
+        (ptr, device_ptr)
     }
+}
+
+#[tokio::main]
+async fn download_url(
+    url: &str,
+    header_size: usize,
+    mut host_ptr: *mut c_void,
+) -> Result<(), reqwest::Error> {
+    let range_header = format!("bytes={}-", header_size);
+    let resp = reqwest::Client::new()
+        .get(url)
+        .header("Range", range_header)
+        .send()
+        .await?;
+
+    let bar = ProgressBar::new(resp.content_length().unwrap());
+
+    let mut stream = resp.bytes_stream();
+    while let Some(item) = stream.next().await {
+        let bytes = item?;
+
+        bar.inc(bytes.len() as u64);
+
+        // write to the ptr
+        unsafe {
+            std::ptr::copy(bytes.as_ptr(), host_ptr as *mut u8, bytes.len());
+        }
+
+        // advance the ptr
+        host_ptr = unsafe {
+            let host_ptr = host_ptr as *mut u8;
+            let host_ptr = host_ptr.add(bytes.len());
+            host_ptr as *mut c_void
+        };
+    }
+
+    Ok(())
+}
+
+#[pyfunction]
+fn download_to_device(url: &str, header_size: usize, data_size: usize) -> PyResult<usize> {
+    cuda_init();
+
+    let (host_ptr, device_ptr) = cuda_malloc_pair(data_size);
+
+    download_url(url, header_size, host_ptr).unwrap();
+
+    // copy from host to device
+    unsafe {
+        let copy_result = cu::cuMemcpyHtoD_v2(device_ptr, host_ptr as *mut c_void, data_size);
+        assert!(
+            copy_result == cu::cudaError_enum::CUDA_SUCCESS,
+            "copy failed: {:?}",
+            copy_result
+        );
+    }
+
+    Ok(device_ptr as usize)
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn model_loader(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(fast_download, m)?)?;
-    m.add_function(wrap_pyfunction!(cuda_malloc, m)?)?;
+    m.add_function(wrap_pyfunction!(download_to_device, m)?)?;
     Ok(())
 }
